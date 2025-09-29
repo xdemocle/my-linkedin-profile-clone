31ff34dad427d2ef4cae02585640ab710387c859
xdemocle <xdemocle@gmail.com>
2025-09-29 12:33:32 +0200

    minor

--- transforms.ts
+++ transforms.ts
@@ -1,18 +1,24 @@
-import Bun from "bun";
+import Bun from 'bun';
+import { readdirSync } from 'fs';
+import { join } from 'path';
+
+const cssDir = './public/assets/css';
+const cssFiles = readdirSync(cssDir)
+  .filter(file => file.endsWith('.css'))
+  .map(file => join(cssDir, file));
+
+console.log(cssFiles);
 
 await Bun.build({
-  entrypoints: [
-    "./public/example.css",
-  ],
-  outdir: "./build",
+  entrypoints: cssFiles,
+  outdir: './build',
   minify: true,
   splitting: true,
-  sourcemap: "none",
-  format: "esm",
-  target: "browser",
+  sourcemap: 'none',
+  format: 'esm',
+  target: 'browser',
   // loader: {
   //   ".css": "css",
   // },
   // --css-chunking
 });
-
