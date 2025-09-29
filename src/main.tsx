80b1132c26a3fca9c4e41be5fd1ef8377c10fee8
xdemocle <xdemocle@gmail.com>
2025-09-29 14:35:31 +0200

    feat: integrate Tailwind CSS and reorganize frontend assets

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,16 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import type { JSX } from "react/jsx-runtime";
+import { createRoot } from 'react-dom/client';
+import type { JSX } from 'react/jsx-runtime';
+import App from './app.tsx';
+import './index.css';
 
-if (typeof window !== "undefined") {
-  const target = document.getElementById("root")!;
-
+if (typeof window !== 'undefined') {
+  const target = document.getElementById('root')!;
   createRoot(target).render(<App />);
 }
 
 export async function prerender(data: JSX.IntrinsicAttributes) {
-  const { renderToStaticMarkup } = await import("react-dom/server");
-  const { parseLinks } = await import("vite-prerender-plugin/parse");
+  const { renderToStaticMarkup } = await import('react-dom/server');
+  const { parseLinks } = await import('vite-prerender-plugin/parse');
 
   const html = await renderToStaticMarkup(<App {...data} />);
   const links = parseLinks(html);
--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,40 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { vitePrerenderPlugin } from "vite-prerender-plugin";
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import { vitePrerenderPlugin } from 'vite-prerender-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     vitePrerenderPlugin({
-      renderTarget: "#root",
+      renderTarget: '#root',
       // additionalPrerenderRoutes: ["/foo", "/bar", "/baz"],
     }),
     {
-      name: "ClosePlugin", // required, will show up in warnings and errors
+      name: 'ClosePlugin', // required, will show up in warnings and errors
 
       // use this to catch errors when building
       buildEnd(error) {
         if (error) {
-          console.error("Error bundling");
+          console.error('Error bundling');
           console.error(error);
           process.exit(1);
         } else {
-          console.log("Build ended");
+          console.log('Build ended');
         }
       },
 
       // use this to catch the end of a build without errors
       closeBundle() {
-        console.log("Bundle closed");
+        console.log('Bundle closed');
         process.exit(0);
       },
     },
   ],
   build: {
-    outDir: "dist/client",
+    outDir: 'dist/client',
     sourcemap: true,
     rollupOptions: {
       output: {
