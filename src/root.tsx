7b119eaaa39e024e93e42813c357289df411a034
xdemocle <xdemocle@gmail.com>
2025-09-29 16:41:52 +0200

    feat: add i18n message files for multiple languages (ar, en, es, fr, it)

--- src/root.tsx
+++ src/root.tsx
@@ -1,14 +1,6 @@
 import { IntlProvider } from 'use-intl';
 import { ProfileLayout } from './components/layout/ProfileLayout';
-
-// You can get the messages from anywhere you like. You can also
-// fetch them from within a component and then render the provider
-// along with your app once you have the messages.
-const messages = {
-  ProfileLayout: {
-    hello: 'Hello {firstName}!',
-  },
-};
+import messages from './messages/en.json';
 
 export function Root() {
   return (
