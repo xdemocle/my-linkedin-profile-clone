ba8cbef2e3cc698db6603aeffd6611e2cbb4aa0e
xdemocle <xdemocle@gmail.com>
2025-10-08 18:59:36 +0200

    refactor: remove redundant ClosePlugin from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -113,25 +113,6 @@ export default defineConfig({
         console.log('Created _headers file for Cloudflare Pages');
       },
     },
-    {
-      name: 'ClosePlugin', // required, will show up in warnings and errors
-
-      // use this to catch errors when building
-      buildEnd(error) {
-        if (error) {
-          console.error('Error bundling');
-          console.error(error);
-          process.exit(1);
-        } else {
-          console.log('Build ended');
-        }
-      },
-
-      // use this to catch the end of a build without errors
-      closeBundle() {
-        process.exit(0);
-      },
-    },
   ],
 
   resolve: {
