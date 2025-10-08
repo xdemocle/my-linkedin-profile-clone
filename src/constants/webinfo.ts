c384728f86c196fd88cd51fa56a3943271cff3d2
xdemocle <xdemocle@gmail.com>
2025-10-08 14:03:00 +0200

    feat: add social media URLs and labels to webinfo constants

--- src/constants/webinfo.ts
+++ src/constants/webinfo.ts
@@ -1,3 +1,32 @@
-export const LINKEDIN_PROFILE_URL = 'www.linkedin.com/in/roccorusso';
+export const LINKEDIN_PROFILE_URL = 'https://www.linkedin.com/in/roccorusso';
 
-export const SUBSTACK_PROFILE_URL = 'substack.com/@xdemocle';
+export const SUBSTACK_PROFILE_URL = 'https://xdemocle.substack.com';
+
+export const CALENDLY_PROFILE_URL = 'https://calendly.com/rocco-russo';
+
+export const TELEGRAM_PROFILE_URL = 'https://t.me/xdemocle';
+
+export const WELLFOUNDED_PROFILE_URL = 'https://wellfound.com/u/rocco-russo';
+
+export const GITHUB_PROFILE_URL = 'https://github.com/xdemocle';
+
+export const X_PROFILE_URL = 'https://x.com/xdemocle';
+
+export const HUGGING_FACE_PROFILE_URL = 'https://huggingface.co/xdemocle';
+
+export const DRIBBBLE_PROFILE_URL = 'https://dribbble.com/xdemocle';
+
+export const CV_REQUEST_URL = 'mailto:hello@rocco.me?subject=New+CV+request+from+website';
+
+export const EMAIL_ADDRESS = 'hello@rocco.me';
+export const LINKEDIN_LABEL = 'LinkedIn';
+export const SUBSTACK_LABEL = 'Substack';
+export const CALENDLY_LABEL = 'Calendar';
+export const TELEGRAM_LABEL = 'Telegram';
+export const WELLFOUNDED_LABEL = 'Wellfound';
+export const GITHUB_LABEL = 'GitHub';
+export const X_LABEL = 'X';
+export const HUGGING_FACE_LABEL = 'Hugging Face';
+export const DRIBBBLE_LABEL = 'Dribbble';
+export const CV_REQUEST_LABEL = 'CV';
+export const EMAIL_LABEL = 'Email';
